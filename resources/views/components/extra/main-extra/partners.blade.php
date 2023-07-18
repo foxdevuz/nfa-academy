873f25c13d13ead2f7a5cadfe52e1fa31d3378a0
foxdevuz <abdullajonovboburjon05@gmail.com>
2023-07-18 23:08:37 +0500

    smal changes and some images deleted

--- resources/views/components/extra/main-extra/partners.blade.php
+++ resources/views/components/extra/main-extra/partners.blade.php
@@ -1,10 +1,42 @@
 <div class="container-fluid bg-img py-5">
     <div class="container py-5">
         <div class="row gx-5 gy-4">
-            <x-extra.main-extra.partner-container/>
-            <x-extra.main-extra.partner-container/>
-            <x-extra.main-extra.partner-container/>
-            <x-extra.main-extra.partner-container/>
+            <div class="col-lg-3 col-md-6">
+                <div class="contatiner">
+                    <div class="d-flex align-items-center justify-content-center" style="width: 100px; height: 100px">
+                        <a href="https://www.fifa.com/fifaplus/en">
+                            <img class="ufaImage" src="/img/FIFA.jpg" alt=""/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="contatiner">
+                    <div class="d-flex align-items-center justify-content-center" style="width: 100px; height: 100px">
+                        <a href="https://www.the-afc.com/en/home.html">
+                            <img class="ufaImage" src="/img/Afc.jpg" alt=""/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="contatiner">
+                    <div class="d-flex align-items-center justify-content-center" style="width: 100px; height: 100px">
+                        <a href="http://ufa.uz/?lang=en">
+                            <img class="ufaImage" src="/img/ufa.png" alt=""/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="contatiner">
+                    <div class="d-flex align-items-center justify-content-center" style="width: 100px; height: 100px">
+                        <a href="/">
+                            <img class="ufaImage" src="/img/NFA.png" alt=""/>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
