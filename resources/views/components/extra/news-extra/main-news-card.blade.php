0dc5167fffe4b8f23bbe94a2d6882ecbf6d1b6c5
foxdevuz <abdullajonovboburjon05@gmail.com>
2023-08-26 15:07:16 +0500

    updated

--- resources/views/components/extra/news-extra/main-news-card.blade.php
+++ resources/views/components/extra/news-extra/main-news-card.blade.php
@@ -3,7 +3,7 @@
     <div class="col-lg-10">
         <div class="d-flex h-100">
             <div class="flex-shrink-0 box">
-                <img class="img-fluid" src="/storage/images/{{ $news->file_1 }}" alt=""
+                <img class="img-fluid" src="/public/storage/images/{{ $news->file_1 }}" alt=""
                     style="width: 150px; height: 85px; object-fit: cover" />
             </div>
             <div class="d-flex flex-column justify-content-center text-start bg-secondary px-4">
--- resources/views/components/main/club-main.blade.php
+++ resources/views/components/main/club-main.blade.php
@@ -1,7 +1,7 @@
 @props(['news', 'event'])
 @unless ($news->isEmpty())
     <div class="container-fluid bg-dark py-5 hero-header"
-        style="background-image: url('/storage/images/{{ $news[0]->file }}'); position:relative;">
+        style="background-image: url('/public/storage/images/{{ $news[0]->file }}'); position:relative;">
         <div class="container py-5">
             <div class="row justify-content-start">
                 <div class="col-lg-8 text-center text-lg-start">
@@ -12,7 +12,7 @@
                 </div>
             </div>
             @if ($event)
-                <button class="event-btn"><img src="/storage/images/{{ $event->event }}" alt="Event in NFA"></button>
+                <button class="event-btn"><img src="/public/storage/images/{{ $event->event }}" alt="Event in NFA"></button>
             @endif
         </div>
     </div>
